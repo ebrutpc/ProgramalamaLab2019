5d4219cb95c8643b301e16256889de77add2f76f
ebrutpc <37660007+ebrutpc@users.noreply.github.com>
2019-04-03 23:37:02 +0300

    Add files via upload

---
+++ BubleSort.py
@@ -0,0 +1,34 @@
+#Random bi dizi olustur
+#Buble Sort ile sırala
+
+import random
+my_array=[]
+def generate_an_array(n):
+    for i in range(n):
+        s=random.randint(0,100)
+        my_array.append(s)
+    return my_array
+
+my_array1=[]
+my_array1=generate_an_array(10)
+
+
+for i in range(len(my_array)-1,0,-1):
+    for j in range(i):
+        if(my_array[j]>my_array[j+1]):
+            t=my_array[j]
+            my_array[j]=my_array[j+1]
+            my_array[j+1]=t
+print(my_array)
+
+def my_search(array_2,item):
+    found=False
+    indis=-1
+    for i in range(len(array_2)):
+        if(array_2[i]==item):
+            found=True
+            indis=i
+    return (found,indis)
+
+
+print(my_search(my_array1,68))
